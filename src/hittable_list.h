206a8e65214a71606bd984eaa70c83c0b9f141ac
JaidenD <jaidend1273@gmail.com>
2025-08-15 21:50:55 -0400

    fixes

---
+++ src/hittable_list.h
@@ -0,0 +1,24 @@
+#ifndef HITTABLE_LIST_H
+#define HITTABLE_LIST_H
+
+#include "hittable.h"
+
+#include <memory>
+#include <vector>
+
+using std::make_shared;
+using std::shared_ptr;
+
+class hittable_list : public hittable {
+    public:
+        std::vector<shared_ptr<hittable>> objects;
+
+        hittable_list() {}
+        hittable_list(shared_ptr<hittable> object) {add(object);}
+
+        void clear() {objects.clear();}
+
+        void add(shared_ptr<hittable> object) {
+            objects.push_back(object);
+        }
+}
